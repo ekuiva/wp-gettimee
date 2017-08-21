c647a1311001083c8c247e136cdee8336d1a347c
Lani Asep Sutisna <lani.asep@lawencon.com>
2017-08-21 08:37:41 +0700

    Update bagian choose gettimee

--- page-hompage.php
+++ page-hompage.php
@@ -102,12 +102,60 @@
 	</div>
 </div> <!--End of Slider-->
 
+<!-- Choose GetTimee -->
+<div class="container margin-top-upper">
+	<div class="row">
+		<div class="col-md-1 col-lg-3"></div>
+		<div class="col-md-10 col-lg-6">
+			<!-- Smooth scrolling target -->
+			<div id="can-do"></div>
+			<h4 class="bold yellow-clr">Giving you whats the best.</h4>
+			<h1 class="uppercase">Why Choose GetTimee</h1>
+		</div>
+		<div class="col-md-1 col-lg-3"></div>
+	</div>
+	<div class="row space-top-high">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<img src="<?php echo get_template_directory_uri()?>/assets/images/icon/icon-transparency.png">
+					<div class="margin-top-up">
+						<h5 class="yellow-clr bold">Transparency</h5>
+						<p>The control of employee’s working time is managed fairly and transparently</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<img src="<?php echo get_template_directory_uri()?>/assets/images/icon/icon-simple.png">
+					<div class="margin-top-up">
+						<h5 class="yellow-clr bold">Simple</h5>
+						<p>The entire system is very easy to set up. GetTimee simplicity will benefit all users</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<img src="<?php echo get_template_directory_uri()?>/assets/images/icon/icon-reliability-hover.png">
+					<div class="margin-top-up">
+						<h5 class="yellow-clr bold">Reliability</h5>
+						<p>It works anywhere with majority devices</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<img src="<?php echo get_template_directory_uri()?>/assets/images/icon/icon-satisfaction.png">
+					<div class="margin-top-up">
+						<h5 class="yellow-clr bold">Satisfaction</h5>
+						<p>Transparent and uncomplicated system will increase satisfaction from all employees</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> <!-- End of Choose GetTimee -->
+
 <!-- Contact Us -->
 <div class="container-fluid bg-contactus margin-top-upper">
 	 <div class="col-md-12 text-center">
-		<a href="#can-do" id="nav-down"><img class="image-scroll-minus" src="<?php echo get_template_directory_uri()?>/assets/images/scroll.png" class="img-responsive"></a>
+		<a href="#contact" id="nav-down"><img class="image-scroll-minus" src="<?php echo get_template_directory_uri()?>/assets/images/scroll.png" class="img-responsive"></a>
 	</div>
-	<div class="container margin-top-upper">
+	<div class="container margin-top-upper" id="contact">
 		<div class="row">
 			<div class="col-md-1 col-lg-3"></div>
 			<div class="col-md-10 col-lg-6">
@@ -122,7 +170,7 @@
 			<a href="#" class="btn-outer"><div class="btn">Contact Us</div></a>
 		</div>
 	</div>
-</div>
+</div> <!-- End of Contact Us -->
 <?php get_footer(); ?>
 
 
