fdb658f7032de5e7fa9c357496043dd6bcfa303e
Lani Asep Sutisna <lani.asep@lawencon.com>
2017-09-06 16:05:14 +0700

    Update wp content item

--- wp-content/themes/gettimee/header.php
+++ wp-content/themes/gettimee/header.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Website GetTimee</title>
+      <title><?php echo get_bloginfo() ?></title>
       <?php wp_head() ?>
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
     
